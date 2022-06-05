9b65176d9e102d20820a69b3b4aec70f0a470334
Sam <noci0001@hz.nl>
2022-06-05 13:31:42 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          The build is now running correctly 
         </a>
       </header>
     </div>
