01e3baf86fd299368d36624979b899c493466d4d
Sam <noci0001@hz.nl>
2022-06-05 13:39:57 +0200

    adding of Appjs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./index.css"
+import Header from "./Header.js"
+import WhatIDo from "./WhatIDo.js"
+import WhoIAM from "./WhoIAM.js"
+import MyWork from "./MyWork.js"
+import Footer from "./Footer.js"
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        BREAKING CHANGES!
-      </header>
+    <div>
+      <Header />
+      <WhatIDo />
+      <WhoIAM />
+      <MyWork/>
+      <Footer />
     </div>
   );
 }
